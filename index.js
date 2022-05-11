ec7c6c29d11ca1822c71ceb527e66b8370dfcfbf
nathan lewan <nathanlewan@gmail.com>
2022-05-11 13:36:44 -0400

    fix server object passing

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ module.exports = (envFileLocation) => {
     
     let globalEnvironment = environment.scaffold_env(envFileLocation);
     let serverWebApp = webserver.scaffold_webserver(globalEnvironment);
-    let socketIoApp = socketIoServer.scaffold_socketIo(globalEnvironment, serverWebApp);
+    let socketIoApp = socketIoServer.scaffold_socketIo(globalEnvironment, serverWebApp.server);
 
+    serverWebApp.app.get('/', (req, res) => {
+        res.send('Test Landing Page')
+    })
+    
     return {
         "globalEnvironment": globalEnvironment,
-        "serverApp": serverWebApp,
+        "serverApp": serverWebApp.app,
         "socketIoApp": socketIoApp
     }
 
--- scaffolds/webserver/webserver.js
+++ scaffolds/webserver/webserver.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const middleware = {
+    authHandler: require('./middlewares/authorization')
+}
 
 exports.scaffold_webserver = (globalConfigs) => {
 
+    let app = express()
     let baseServer = false;
     let Server = false;
 
     switch (globalConfigs.webProtocol) {
 
         case "http": {
-
             baseServer = require('http');
-            Server = baseServer.createServer(express());
+            Server = baseServer.createServer(app);
             Server.listen(globalConfigs.port, () => {
                 console.log(`server running at: ${globalConfigs.webProtocol}://${globalConfigs.hostname}:${globalConfigs.port}`)
             });
@@ -18,5 +21,10 @@ exports.scaffold_webserver = (globalConfigs) => {
         }
     }
 
-    return Server;
+
+
+    return {
+        app: app,
+        server: Server
+    }
 }
